bc0edc58ccff46a856816299e9c60d4dbc8bbef2
shivam-231093 <155953063+shivam-231093@users.noreply.github.com>
2025-07-19 00:31:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMR0pheSA9p0562tPo6lm2AhormlHbiSk",
-  authDomain: "vibe-coding-31d71.firebaseapp.com",
-  projectId: "vibe-coding-31d71",
-  storageBucket: "vibe-coding-31d71.firebasestorage.app",
-  messagingSenderId: "54506788879",
-  appId: "1:54506788879:web:51fc966e6eef44bbe22b7a",
-  measurementId: "G-MH4N1YHEYQ"
+  apiKey: ,
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
